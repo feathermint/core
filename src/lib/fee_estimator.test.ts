060a5cf5be7fa08ff8d838ea335b0f4b416b4d8d
Sebastian <sebastianwijkhuizen@gmail.com>
2023-09-06 11:44:46 +0200

    test(FeeEstimator): use updated MockCollection

--- src/lib/fee_estimator.test.ts
+++ src/lib/fee_estimator.test.ts
@@ -1,13 +1,9 @@
+import { Document } from "@feathermint/mongo-connect";
 import chai, { expect } from "chai";
 import chaiAsPromised from "chai-as-promised";
-import { EventReporter } from "../lib/event_reporter";
-import { MockCollection, MockDataSource } from "../mocks";
+import { MockCollection, MockDataSource, MockEventReporter } from "../mocks";
 import { GasPrice, MaticPrice } from "../types/core";
-import {
-  FeeEstimator,
-  InvalidGasFeeMarginError,
-  PriceNotFoundError,
-} from "./fee_estimator";
+import { FeeEstimator, PriceNotFoundError } from "./fee_estimator";
 
 chai.use(chaiAsPromised);
 
@@ -24,53 +20,32 @@ const maticPriceDoc: MaticPrice = {
   lastUpdated: new Date(),
 };
 
-const prices: MockCollection = {
+const prices = new MockCollection<Document>({
   findOne(...args: unknown[]) {
     const arg = args[0] as { name: string };
     if (arg.name === "gas") return Promise.resolve(gasPriceDoc);
     if (arg.name === "matic") return Promise.resolve(maticPriceDoc);
     return Promise.reject("Invalid argument");
   },
-};
+});
 
 const deps = {
   dataSource: new MockDataSource({ prices }),
-  eventReporter: {
-    captureException: () => "eventId",
-    captureMessage: () => "eventId",
-  } as EventReporter,
+  eventReporter: new MockEventReporter(),
   platformFee: 0.02,
-  gasFeeMargin: 0,
 };
 
 describe("FeeEstimator", () => {
   describe("#init", () => {
     it("throws if the the gas price document is not found", async () => {
-      await expect(
-        FeeEstimator.init({
-          ...deps,
-          dataSource: new MockDataSource({
-            prices: { findOne: () => Promise.resolve(null) },
-          }),
-        }),
-      ).to.be.rejectedWith(PriceNotFoundError);
-    });
-
-    it("throws if the the matic price document is not found", async () => {
-      const prices: MockCollection = {
+      const prices = new MockCollection({
         findOne(...args: unknown[]) {
           const arg = args[0] as { name: string };
-          if (arg.name === "gas")
-            return Promise.resolve<GasPrice>({
-              name: "gas",
-              unit: "wei",
-              price: "0x17b79a6e99",
-              lastUpdated: new Date(),
-            });
-          if (arg.name === "matic") return Promise.resolve(null);
+          if (arg.name === "gas") return Promise.resolve(null);
+          if (arg.name === "matic") return Promise.resolve(maticPriceDoc);
           return Promise.reject("Invalid argument");
         },
-      };
+      });
 
       await expect(
         FeeEstimator.init({
@@ -80,14 +55,22 @@ describe("FeeEstimator", () => {
       ).to.be.rejectedWith(PriceNotFoundError);
     });
 
-    it("throws if the the gas price margin is invalid", async () => {
-      await expect(
-        FeeEstimator.init({ ...deps, gasFeeMargin: -0.5 }),
-      ).to.be.rejectedWith(InvalidGasFeeMarginError);
+    it("throws if the the matic price document is not found", async () => {
+      const prices = new MockCollection({
+        findOne(...args: unknown[]) {
+          const arg = args[0] as { name: string };
+          if (arg.name === "gas") return Promise.resolve(gasPriceDoc);
+          if (arg.name === "matic") return Promise.resolve(null);
+          return Promise.reject("Invalid argument");
+        },
+      });
 
       await expect(
-        FeeEstimator.init({ ...deps, gasFeeMargin: 1.5 }),
-      ).to.be.rejectedWith(InvalidGasFeeMarginError);
+        FeeEstimator.init({
+          ...deps,
+          dataSource: new MockDataSource({ prices }),
+        }),
+      ).to.be.rejectedWith(PriceNotFoundError);
     });
   });
 
@@ -124,20 +107,6 @@ describe("FeeEstimator", () => {
 
       const result = fe.gasFee(gasUnits);
       expect(result).to.eq(BigInt(gasUnits) * fe.gasPrice);
-    });
-  });
-
-  describe("#reservedAmount", () => {
-    it("returns the reserved amount in gwei for an operation", async () => {
-      const fe = await FeeEstimator.init(deps);
-
-      let gasUnits = fe.gasUnits("createToken");
-      let result = fe.reservedAmount(gasUnits);
-      expect(result).to.eq(39_633_283);
-
-      gasUnits = fe.gasUnits("safeBatchTransferFrom", 5);
-      result = fe.reservedAmount(gasUnits);
-      expect(result).to.eq(42_733_478);
     });
   });
 });
