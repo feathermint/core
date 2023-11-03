03c76e5179b5663b9df1c784037c1be7062aa666
Sebastian <sebastianwijkhuizen@gmail.com>
2023-11-03 19:26:10 +0100

    test: update MockEventReporter

--- src/mocks/event_reporter.ts
+++ src/mocks/event_reporter.ts
@@ -1,11 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { EventReporter } from "..";
 
 export class MockEventReporter implements Required<EventReporter> {
-  captureException(): string {
+  captureException(exception?: unknown): string {
     throw new Error("Method not implemented.");
   }
 
-  captureMessage(): string {
+  captureMessage(message?: string): string {
     throw new Error("Method not implemented.");
   }
 }
