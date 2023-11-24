f71f9de9bbe0304aa21ff52f3d7a9de66aa1ee55
Sebastian <sebastianwijkhuizen@gmail.com>
2023-11-24 12:31:52 +0100

    test: add MockStatusService

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,3 +1,4 @@
 export { MockCollection, MockCursor, MockDataSource } from "./data_source";
 export { MockEventReporter } from "./event_reporter";
 export { MockFeeEstimator } from "./fee_estimator";
+export { MockStatusService } from "./status_service";
