249cf1797cf8df772d6aec120de65e7f281fdc31
Sebastian <sebastianwijkhuizen@gmail.com>
2023-12-04 15:11:10 +0100

    fix: update package exports
    Re-export all types used by the DataSource class.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { DataSource } from "./lib/data_source";
+export * from "./lib/data_source";
 export { EventReporter } from "./lib/event_reporter";
 export { FeeEstimator } from "./lib/fee_estimator";
 export { StatusService } from "./lib/status_service";
