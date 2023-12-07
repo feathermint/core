3b0c4fdfe7f9bc240cedb9c5ee95aaadd5768759
Sebastian <sebastianwijkhuizen@gmail.com>
2023-12-07 21:32:28 +0100

    test(FeeEstimator): rewrite tests

--- src/lib/fee_estimator.test.ts
+++ src/lib/fee_estimator.test.ts
@@ -1,77 +1,99 @@
-import type { Document } from "@feathermint/mongo-connect";
+import { Redis } from "@feathermint/redis-connect";
 import chai, { expect } from "chai";
 import chaiAsPromised from "chai-as-promised";
-import { MockCollection, MockDataSource, MockEventReporter } from "../mocks";
-import type { GasPrice, MaticPrice } from "../types/domain";
-import { FeeEstimator, PriceNotFoundError } from "./fee_estimator";
+import { MockEventReporter } from "../mocks";
+import {
+  FeeEstimator,
+  InvalidPriceError,
+  RedisKeyNotFoundError,
+} from "./fee_estimator";
 
 chai.use(chaiAsPromised);
 
-const gasPriceDoc: GasPrice = {
-  name: "gas",
-  unit: "wei",
+const gasPrice = JSON.stringify({
   baseFeePerGas: 2 ** 32,
   maxPriorityFeePerGas: 2 ** 31,
-  lastUpdated: new Date(),
-};
-const maticPriceDoc: MaticPrice = {
-  name: "matic",
-  unit: "usd",
+  timestamp: Date.now(),
+});
+const maticPrice = JSON.stringify({
   price: 0.8,
-  lastUpdated: new Date(),
-};
-
-const prices = new MockCollection<Document>({
-  findOne(...args: unknown[]) {
-    const arg = args[0] as { name: string };
-    if (arg.name === "gas") return Promise.resolve(gasPriceDoc);
-    if (arg.name === "matic") return Promise.resolve(maticPriceDoc);
-    return Promise.reject("Invalid argument");
-  },
+  timestamp: Date.now(),
 });
 
 const deps = {
-  dataSource: new MockDataSource({ prices }),
   eventReporter: new MockEventReporter(),
   platformFee: 0.02,
 };
 
 describe("FeeEstimator", () => {
   describe("#init", () => {
-    it("throws if the the gas price document is not found", async () => {
-      const prices = new MockCollection({
-        findOne(...args: unknown[]) {
-          const arg = args[0] as { name: string };
-          if (arg.name === "gas") return Promise.resolve(null);
-          if (arg.name === "matic") return Promise.resolve(maticPriceDoc);
-          return Promise.reject("Invalid argument");
-        },
-      });
-
+    it("throws if the matic price is not found", async () => {
       await expect(
         FeeEstimator.init({
           ...deps,
-          dataSource: new MockDataSource({ prices }),
+          redis: {
+            mget: () => Promise.resolve([null, gasPrice]),
+          } as Redis,
         }),
-      ).to.be.rejectedWith(PriceNotFoundError);
+      ).to.be.rejectedWith(RedisKeyNotFoundError);
     });
 
-    it("throws if the the matic price document is not found", async () => {
-      const prices = new MockCollection({
-        findOne(...args: unknown[]) {
-          const arg = args[0] as { name: string };
-          if (arg.name === "gas") return Promise.resolve(gasPriceDoc);
-          if (arg.name === "matic") return Promise.resolve(null);
-          return Promise.reject("Invalid argument");
-        },
-      });
-
+    it("throws if the gas price is not found", async () => {
       await expect(
         FeeEstimator.init({
           ...deps,
-          dataSource: new MockDataSource({ prices }),
+          redis: {
+            mget: () => Promise.resolve([maticPrice, null]),
+          } as Redis,
         }),
-      ).to.be.rejectedWith(PriceNotFoundError);
+      ).to.be.rejectedWith(RedisKeyNotFoundError);
+    });
+
+    it("throws if the matic price is not valid", async () => {
+      await expect(
+        FeeEstimator.init({
+          ...deps,
+          redis: {
+            mget: () => {
+              const maticPrice = JSON.stringify({
+                price: null,
+                timestamp: Date.now(),
+              });
+              return Promise.resolve([maticPrice, gasPrice]);
+            },
+          } as Redis,
+        }),
+      ).to.be.rejectedWith(InvalidPriceError);
+    });
+
+    it("throws if the gas price is not valid", async () => {
+      await expect(
+        FeeEstimator.init({
+          ...deps,
+          redis: {
+            mget: () => {
+              const gasPrice = JSON.stringify({
+                baseFeePerGas: 2 ** 32,
+                timestamp: Date.now(),
+              });
+              return Promise.resolve([maticPrice, gasPrice]);
+            },
+          } as Redis,
+        }),
+      ).to.be.rejectedWith(InvalidPriceError);
+    });
+
+    it("returns a FeeEstimator instance", async () => {
+      await expect(
+        FeeEstimator.init({
+          ...deps,
+          redis: {
+            mget: () => Promise.resolve([maticPrice, gasPrice]),
+            subscribe: () => Promise.resolve(null),
+            on() {},
+          } as unknown as Redis,
+        }),
+      ).to.eventually.be.instanceOf(FeeEstimator);
     });
   });
 });
