25c0be97b9d209181541aaff69e585df46f1d1fb
Sebastian <sebastianwijkhuizen@gmail.com>
2023-12-09 13:25:28 +0100

    feat!: remove StatusService class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export * from "./lib/data_source";
 export { EventReporter } from "./lib/event_reporter";
 export { FeeEstimator } from "./lib/fee_estimator";
-export { StatusService } from "./lib/status_service";
 export * from "./lib/utils";
 export * as mocks from "./mocks";
 export * from "./types/domain";
--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,4 +1,3 @@
 export { MockCollection, MockCursor, MockDataSource } from "./data_source";
 export { MockEventReporter } from "./event_reporter";
 export { MockFeeEstimator } from "./fee_estimator";
-export { MockStatusService } from "./status_service";
