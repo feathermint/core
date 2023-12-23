070c3a6e3fa4ecf1218b255abe6779e1a50f4f11
Sebastian <sebastianwijkhuizen@gmail.com>
2023-12-23 14:34:23 +0100

    feat!: remove matic related constants

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -2,12 +2,10 @@ export const ONE_GWEI = BigInt(10 ** 9);
 export const ONE_MATIC = BigInt(10 ** 9) * ONE_GWEI;
 export const REDIS = {
   KEYS: {
-    maticPrice: "price:matic",
     gasPrice: "price:gas",
     txjobs: "jobs:tx",
   } as const,
   CHANNELS: {
-    matic: "channel:matic",
     gas: "channel:gas",
   } as const,
 };
