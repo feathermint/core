099f62e1baa1b3ec6ed1a6a64342f45b0a827982
Sebastian <sebastianwijkhuizen@gmail.com>
2023-12-23 14:34:47 +0100

    feat!: remove FeeEstimator and MockFeeEstimator classes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from "./lib/data_source";
 export { EventReporter } from "./lib/event_reporter";
-export { FeeEstimator } from "./lib/fee_estimator";
 export * from "./lib/utils";
 export * as mocks from "./mocks";
 export * from "./types/domain";
--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,3 +1,2 @@
 export { MockCollection, MockCursor, MockDataSource } from "./data_source";
 export { MockEventReporter } from "./event_reporter";
-export { MockFeeEstimator } from "./fee_estimator";
